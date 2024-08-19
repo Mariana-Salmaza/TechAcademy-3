2cd739505e59d4c57b566ce9e70000137c0574fd
Mariana Salmaza <marianasalmaza.jobs@gmail.com>
2024-08-19 00:31:33 -0300

    creates base project

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jogo em java</title>
+
+    <link rel="stylesheet" href="css/style.scss">
+</head>
+<body>
+
+<header>
+        <div class="logo">
+            <img src="img/logo.png" alt="Logo" class="width">
+        </div>
+    </header>
+
+    <div class="container">
+        <textarea class="card" placeholder="Digite seu texto aqui"></textarea>
+    </div>
+
+
+
+
+</body>
+</html>
